c20fa645ee89832d2fd83523f2d4c1651a661c68
Bob Roberts <cardsfanbob@gmail.com>
2020-07-29 20:58:36 -0500

    Finished the orders list, search, paginate and details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,26 @@
 class OrdersController < ApplicationController
+  include Pagy::Backend
   before_action :find_order!, only: :show
 
   def index
+    @q = Order.ransack(params[:q])
+    @orders = @q.result(distinct: true)
+    @pagy, @orders = pagy(@orders, items: 10)
   end
 
   def show
+  end
+
+  def cancel_order
+    @order = Order.find_by!(number: params[:order_number])
+    @order.update state: 'canceled'
+    redirect_to action: :index
+  end
+
+  def reset_search
+    @q = Order.ransack(nil)
+    @orders = @q.result(distinct: true)
+    redirect_to action: :index
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'admin#index'
 
-  resources :orders, only: [:index, :show], param: :number
+  resources :orders, only: [:index, :show], param: :number do
+    get :cancel_order
+    collection do
+      get :reset_search
+    end
+  end
 
   resources :reports, only: :index do
   end
